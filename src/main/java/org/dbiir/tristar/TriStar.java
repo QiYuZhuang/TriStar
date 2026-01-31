f4bbda7437120f7eda5aa13756172550d2a5b99a
QiYu Zhuang <qyzhuang@ruc.edu.cn>
2026-01-31 22:01:48 +0800

    fix bug on serializable execution

--- src/main/java/org/dbiir/tristar/TriStar.java
+++ src/main/java/org/dbiir/tristar/TriStar.java
@@ -179,7 +179,8 @@ public class TriStar {
         if (wrkld.getConcurrencyControlType() == CCType.RC_TAILOR ||
                 wrkld.getConcurrencyControlType() == CCType.SI_TAILOR ||
                 wrkld.getConcurrencyControlType() == CCType.DYNAMIC ||
-                wrkld.getConcurrencyControlType() == CCType.FS) {
+                wrkld.getConcurrencyControlType() == CCType.FS ||
+                wrkld.getConcurrencyControlType() == CCType.SER) {
             Socket socket = null;
             PrintWriter out;
             try {
